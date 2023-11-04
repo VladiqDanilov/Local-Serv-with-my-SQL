1142af0dc5b7977dd5c74f87011fd9d733fbe21b
Vladiq <vladislav.daniloff2014@gmail.com>
2023-11-04 16:16:46 +0300

    add port param to serv.py

--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 import http.server
 import socketserver
+import sys
 
-port = 888
+#по умолч
+default_port = 8888
 
+# Если передан арг командной
+if len(sys.argv) > 1:
+    try:
+        port = int(sys.argv[1])
+    except ValueError:
+        print("Неверный порт. Используется порт по умолчанию:", default_port)
+        port = default_port
+else:
+    port = default_port
 
+# Указываем имя файла
 index_file = "index.html"
 
+#обработчик запросов
 handler = http.server.SimpleHTTPRequestHandler
+
+# Запускаем сервер
 with socketserver.TCPServer(("", port), handler) as httpd:
     print(f"Сервер запущен на порту {port}")
     httpd.serve_forever()
